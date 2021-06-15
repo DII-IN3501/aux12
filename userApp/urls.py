b672fbcf67108abff3a7ce1af57b7de4260bb234
Benjamín Hurtado Torrealba <benjamin.hurtado@ing.uchile.cl>
2021-06-15 01:57:17 -0400

    Sistema de usuarios agregado

---
+++ userApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from userApp.views import registerView, loginView, logoutView, auth, newuser
+
+urlpatterns = [
+    path('register/', registerView, name="register"),
+    path('newuser/', newuser, name="newuser"),
+    path('login/', loginView, name="login"),
+    path('auth/', auth, name='auth'),
+    path('logout/', logoutView, name='logout')
+]
--- userApp/views.py
+++ userApp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+from django.contrib.auth.models import User
+from userApp.models import Cliente
+
+from userApp.forms import IniciarSesion, Registro
+
+def registerView(request):
+    form = Registro()
+    return render(request, 'userApp/register.html', {'form': form})
+
+def newuser(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    telefono = request.POST['telefono']
+    direccion = request.POST['direccion']
+    user = User.objects.create_user(username=username, password=password)
+
+    cliente = Cliente(user=user, telefono=telefono, direccion=direccion)
+    cliente.save()
+
+    return render(request, 'mainApp/index.html')
+
+def loginView(request):
+    form = IniciarSesion()
+    return render(request, 'userApp/login.html', {'form': form})
+
+def auth(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    usuario = authenticate(username=username, password=password)
+    login(request, usuario)
+    return render(request, 'mainApp/index.html')
+
+def logoutView(request):
+    logout(request)
+    return render(request, 'mainApp/index.html')
